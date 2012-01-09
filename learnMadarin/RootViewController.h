d4e7b6686bd0f01cbcc951fb99c3f1863fceab9b
Yuanfeng Gao <gyfelton@gmail.com>
2012-01-09 20:22:26 +0800

    Initial Commit

---
+++ learnMadarin/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  learnMadarin
+//
+//  Created by Yuanfeng on 12-01-09.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ learnMadarin/learnMadarinAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  learnMadarinAppDelegate.h
+//  learnMadarin
+//
+//  Created by Yuanfeng on 12-01-09.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface learnMadarinAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
